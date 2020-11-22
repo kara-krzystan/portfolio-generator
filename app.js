dd86daf1980509a853289b497aaa2b212facada9
Kara Krzystan <kara.krzystan@gmail.com>
2020-11-22 15:27:14 -0700

    Updated template function input

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
-
 // fs.writeFile('./index.html', pageHTML, err => {
 //     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
+// console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
 const promptUser = () => {
@@ -149,5 +147,11 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      console.log(portfolioData);
-  });
+    const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    // if (err) throw new Error(err);
+
+    // console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+});
